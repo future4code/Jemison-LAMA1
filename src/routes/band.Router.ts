2c7e371b5a6fb95afe855f18490191bebb1d470f
RenatoAlexandrini <re.alexandrini@gmail.com>
2023-03-20 02:31:17 -0300

    erro deploy bandController

--- src/routes/band.Router.ts
+++ src/routes/band.Router.ts
@@ -1,18 +1,20 @@
-import { ShowDatabase } from './../data/showDatabase';
+import { BandController } from './../controller/bandController';
 import { Authenticator } from './../services/authenticator';
-import express from "express";
-import {BandsBusiness} from "../business/BandBusiness";
-import { BandDatabase } from "../data/bandDatabase";
+import { ShowDatabase } from './../data/showDatabase';
+import { BandDatabase } from './../data/bandDatabase';
+import { BandBusiness } from './../business/bandBusiness';
+import express from 'express';
 import { IdGenerator } from '../services/idGenerator';
-import { BandController } from "../controller/bandController"
+
 
 export const bandRouter = express.Router();
 
-const bandDatabase = new BandDatabase()
-const showDatabase = new ShowDatabase()
-const authenticator = new Authenticator()
-const idGenerator = new IdGenerator()
-const bandBusiness = new BandsBusiness(bandDatabase,showDatabase, authenticator, idGenerator)
+const bandDataBase = new BandDatabase();
+const showDatabase = new ShowDatabase();
+const autheticator = new Authenticator();
+const idGenerator = new IdGenerator();
+
+const bandBusiness = new BandBusiness(bandDataBase, showDatabase, autheticator, idGenerator);
 const bandController = new BandController(bandBusiness)
 
 bandRouter.get('/details',(req,res)=> bandController.getBandDetails(req, res))
